2e7fef6cd0ecc374c276b3fa805a96bcdbca79b8
Jason Walsh <j@wal.sh>
2015-07-31 10:59:54 -0400

    Frequency

---
+++ src/static/js/index.js
@@ -0,0 +1,20 @@
+console.log('index.js');
+
+var frequency = function(text) {
+    var result = text
+            .split(' ')
+            .reduce(function(p, c, i) {
+                p[c] ? p[c]++ : p[c] = 1;
+                return p;
+            }, {});
+    return result;
+};
+
+var test = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam'
+        + ' non euismod nulla. In mattis ex at auctor pellentesque.'
+        + ' Interdum et malesuada fames ac ante ipsum primis in'
+        + ' faucibus. Quisque quis lectus molestie, placerat neque a,'
+        + ' sagittis sem. Integer id sem id tortor dignissim vehicula.'
+        + ' Integer efficitur, odio eget efficitur rutrum.';
+
+console.log(frequency(test));
